4231ee2eeb38585417bba5a66602c0226c310a2c
genival.junior <genivaljunior86@gmail.com>
2023-05-07 13:43:55 -0300

    Initial Changes

--- src/api/order/content-types/order/lifecycles.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  beforeCreate: async (event) => {
-    const { data } = event.params;
-    if (data.has_custom_price) {
-      data.final_price = data.custom_price;
-    }
-  }
-}
